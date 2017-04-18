1cfab8a23ed31eabde616014746547ea62294b19
Michael Ballard <ballard.michael9@gmail.com>
2017-04-18 12:35:23 -0400

    Swift OOP

---
+++ swift_OOP.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class User {
+    
+    // Attributes of my class
+    
+    var name = ""
+    var age = 0
+    var favoriteMovie = ""
+    var favoriteSong = " "
+    var isDeveloper = false
+}
+
+
+// Creating a new user and setting attributes manually
+var newUser = User()
+newUser.name = "Michael Ballard"
+newUser.age = 26
+newUser.favoriteSong = "Rise and Shine, J Cole"
+newUser.isDeveloper = true
+print(newUser.name)
+
+

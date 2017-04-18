54468270dc466f73146af7bd43bfb5d1c8789e2a
Michael Ballard <ballard.michael9@gmail.com>
2017-04-18 12:54:32 -0400

    SimpleCalcPractice

---
+++ SimpleCalculator/SimpleCalculator/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Michael Ballard on 4/18/17.
+//  Copyright © 2017 Michael Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstNumberTxt: UITextField!
+    
+    @IBOutlet var secondNumberTxt: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func sumBtn(_ sender: Any) {
+    }
+    
+
+    @IBAction func subtractionBtn(_ sender: Any) {
+    }
+
+    @IBAction func mutiplicationBtn(_ sender: Any) {
+    }
+    
+    @IBAction func divisionBtn(_ sender: Any) {
+    }
+   
+    @IBAction func moduloBtn(_ sender: Any) {
+
+    }
+    
+    
+
+}
+
